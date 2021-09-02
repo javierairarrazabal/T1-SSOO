7ebc2157cd28697d6977bec43b5d5e8fdabe226c
jirarrazabal <jirarrazabal@uc.cl>
2021-09-02 00:25:47 -0400

    pls

--- src/dccuber/main.c
+++ src/dccuber/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 #include "../file_manager/manager.h"
 
--- src/semaforo/main.c
+++ src/semaforo/main.c
@@ -3,6 +3,5 @@
 
 int main(int argc, char const *argv[])
 {
-  printf("YAPUEEEEE CTM\n");
   printf("I'm the SEMAFORO process and my PID is: %i\n", getpid());
 }
