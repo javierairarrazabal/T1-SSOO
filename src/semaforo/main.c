49e2e8de6a90c8a0f5c6db4fa83fc70d5cdfb640
jirarrazabal <jirarrazabal@uc.cl>
2021-09-02 15:04:35 -0400

    o

--- src/semaforo/main.c
+++ src/semaforo/main.c
@@ -4,6 +4,6 @@
 int main(int argc, char const *argv[])
 {
   printf("I'm the SEMAFORO process and my PID is: %i\n", getpid());
-  printf("delay semaforo %i: %i\n", argv[1], argv[2]);
-  printf("parent semaforo %i: %i\n", argv[1], argv[3]);
+  printf("delay semaforo %c: %c\n", *argv[1], *argv[2]);
+  printf("parent semaforo %c: %c\n", *argv[1], *argv[3]);
 }
