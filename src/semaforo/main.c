cd5a53caacb3e7b50ab03a44481d208095a9af09
jirarrazabal <jirarrazabal@uc.cl>
2021-09-03 00:21:51 -0400

    señal

--- src/semaforo/main.c
+++ src/semaforo/main.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdbool.h>
 
 int main(int argc, char const *argv[])
 {
   int parent_pid = strtol(argv[3], NULL, 10);
   int id = strtol(argv[1], NULL, 10);
   int delay = strtol(argv[2], NULL, 10);
+  bool prendido = true;
   printf("I'm the SEMAFORO process and my PID is: %i\n", getpid());
-  printf("delay semaforo %d: %d\n", id, delay);
-  printf("parent semaforo %d: %d\n", id, parent_pid);
+  while (true)
+  {
+    sleep(delay);
+    prendido = !prendido;
+    printf("SEMAFORO prendido: %d\n", prendido);
+  };
+  
 }
