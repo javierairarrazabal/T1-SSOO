e9b65f03dd3745649456736294d7a1e2c91ff169
jirarrazabal <jirarrazabal@uc.cl>
2021-09-03 15:37:48 -0400

    okkk probar señal

--- src/repartidor/main.c
+++ src/repartidor/main.c
@@ -14,16 +14,16 @@ int cant_turnos[4];
 int distancia = 0;
 int ubicacion_bodega;
 
-// void handle_sigusr1(int sig, siginfo_t *siginfo, void *context)
-// {
-//   int number_received = siginfo->si_value.sival_int;
-//   estado_semaforos[number_received] = !estado_semaforos[number_received];
-//   printf("Repartidor de pid %d: Recibi semaforo id %i en estado %i\n", getpid(), number_received, estado_semaforos[number_received]);
-// }
+void handle_sigusr1(int sig, siginfo_t *siginfo, void *context)
+{
+  int number_received = siginfo->si_value.sival_int;
+  estado_semaforos[number_received] = !estado_semaforos[number_received];
+  printf("Repartidor de pid %d: Recibi semaforo id %i en estado %i\n", getpid(), number_received, estado_semaforos[number_received]);
+}
 
 int main(int argc, char const *argv[])
 {
-  //connect_sigaction(SIGUSR1, handle_sigusr1);
+  connect_sigaction(SIGUSR1, handle_sigusr1);
   int id = strtol(argv[1], NULL, 10);
   estado_semaforos[0] = strtol(argv[2], NULL, 10);
   estado_semaforos[1] = strtol(argv[3], NULL, 10);
@@ -32,7 +32,6 @@ int main(int argc, char const *argv[])
   ubicacion_semaforos[1] = strtol(argv[6], NULL, 10);
   ubicacion_semaforos[2] = strtol(argv[7], NULL, 10);
   ubicacion_bodega = strtol(argv[8], NULL, 10);
-  //connect_sigaction(SIGUSR1, handle_sigusr1);
   printf("I'm the REPARTIDOR process and my PID is: %i, my id is %i\n", getpid(), id);
   printf("semaforo 1 %i, semaforo 2 %i, semaforo 3 %i, bodega %i\n", ubicacion_semaforos[0], ubicacion_semaforos[1], ubicacion_semaforos[2], ubicacion_bodega);
   while (true)
