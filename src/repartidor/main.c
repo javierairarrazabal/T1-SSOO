9893b185378c04ef6493a729ef889ebb281a819d
jirarrazabal <jirarrazabal@uc.cl>
2021-09-03 00:10:13 -0400

    perrito

--- src/repartidor/main.c
+++ src/repartidor/main.c
@@ -5,4 +5,5 @@ int main(int argc, char const *argv[])
 {
   int id = strtol(argv[1], NULL, 10);
   printf("I'm the REPARTIDOR process and my PID is: %i, my id is %i\n", getpid(), id);
+  sleep(10);
 }
