38ac7022478252de9c07e025c0fe31515378c960
jirarrazabal <jirarrazabal@uc.cl>
2021-09-04 14:40:02 -0400

    shitumare

--- src/dccuber/main.c
+++ src/dccuber/main.c
@@ -35,8 +35,8 @@ void handle_sigalrm(int sig)
   for (int i = 1; i < cant_repartidores; i++)
   {
     printf("ID: %i\n", i);
-    printf("cantidad menos 1: %i\n", cant_repartidores-1);
-    printf("comparación: %i\n", i == cant_repartidores-1);
+    // printf("cantidad menos 1: %i\n", cant_repartidores-1);
+    // printf("comparación: %i\n", i == cant_repartidores-1);
     repartidores_pid[i] = fork();
     if (!repartidores_pid[i])
     {
@@ -52,6 +52,7 @@ void handle_sigalrm(int sig)
       if (i < cant_repartidores-1)
       {
         sprintf(&ultimo, "%d", 0);
+        printf("holaa");
       } else {
         printf("SE MANDO ULTIMOOOOO\n");
         sprintf(&ultimo, "%d", 1);
