93b03f1fda0ffcc29db3a554b2686e14dd9029b2
jirarrazabal <jirarrazabal@uc.cl>
2021-09-03 20:49:22 -0400

    perro

--- src/dccuber/main.c
+++ src/dccuber/main.c
@@ -162,6 +162,7 @@ int main(int argc, char const *argv[])
       signal(SIGUSR2, handle_sigusr2);
       alarm(strtol(data_in->lines[1][0], NULL, 10));
       connect_sigaction(SIGUSR1, handle_sigusr1);
+      printf("pid rep %i\n", repartidores_pid[0]);
       waitpid(repartidores_pid[0], &status_fabrica, 0);
     }
   }
