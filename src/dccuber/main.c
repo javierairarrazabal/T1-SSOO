b2bbab241ceaf45122794f383dcbe7ec8f26b371
jirarrazabal <jirarrazabal@uc.cl>
2021-09-05 01:10:25 -0300

    ojala

--- src/dccuber/main.c
+++ src/dccuber/main.c
@@ -182,10 +182,17 @@ int main(int argc, char const *argv[])
       alarm(strtol(data_in->lines[1][0], NULL, 10));
       connect_sigaction(SIGUSR1, handle_sigusr1);
       while(!repartidores_pid[cant_repartidores-1]);
-      for (int i = 0; i < cant_repartidores; i++) {
-        waitpid(repartidores_pid[i], &status_fabrica, 0);
+      pid_t all_finish;
+      while (true) {
+        all_finish = wait(NULL);
+        if (all_finish == -1) {
+          exit(0);
+        }
       }
-      while(true);
+      // for (int i = 0; i < cant_repartidores; i++) {
+      //   waitpid(repartidores_pid[i], &status_fabrica, 0);
+      // }
+      // while(true);
     }
   }
   else
